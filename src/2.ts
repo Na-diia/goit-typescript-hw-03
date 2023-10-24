e3c3caf113e005d9123085d0ad9f3cfd13ea6725
Pekaruk Nadiia <108739131+Na-diia@users.noreply.github.com>
2023-10-24 13:09:34 +0300

    add hw-3

--- src/2.ts
+++ src/2.ts
@@ -10,30 +10,27 @@
 
   Клас Manager повинен бути підклас класу Employee
 
-  Необхідно реалізувати в класі Manager конструктор, який викликатиме конструктор суперкласу та збільшуватиме salary на 10000.
+  Необхідно реалізувати в класі Manager конструктор, який викликатиме 
+  конструктор суперкласу та збільшуватиме salary на 10000.
 
 */
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
+  constructor(public name: string, private department: string, protected salary: number) {};
 
   getEmployeeDetails() {
-    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
-  }
-}
+    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary + 10000}`;
+  };
+};
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
-}
+  constructor (public name: string, protected salary: number) {
+    super(name, '', salary);
+    super.getEmployeeDetails();
+  }
+};
 
-
-export {};
+const manager = new Manager ('Sophiia', 12);
+console.log(manager.getEmployeeDetails());
